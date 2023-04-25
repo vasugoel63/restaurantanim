47d55b951fa1c4c2ba3e2e15a5a98f6f51050857
Vasugoel <vasugoel4308@gmail.com>
2023-04-25 17:27:32 +0530

    added button

--- index.js
+++ index.js
@@ -83,3 +83,23 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+  // console.log(document.documentElement.scrollTop);
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
